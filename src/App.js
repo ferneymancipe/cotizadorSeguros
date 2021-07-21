a10b6f6316e149dae3ef2b2ceb62d437cdbe973d
Edgar Ferney Mancipe Piragauta <1012354001@colcomercio.com.co>
2021-07-21 14:46:45 -0500

    Primer commit cotizador seguros+

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Resumen from './components/Resumen';
+import Resultado from './components/Resultado';
+import Spinner from './components/Spinner';
+
+import styled from '@emotion/styled';
+import { Fragment } from 'react';
+
+const Contenedor = styled.div`
+   max-width: 600px;
+   margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+   background-color: #FFF;
+   padding: 3rem;
+`;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   const [ resumen, guardarResumen ] = useState({
+      cotizacion: 0,
+      datos: {
+         marca: '',
+         year: '',
+         plan: ''
+      }
+   });
+
+   const [ cargando, guardarCargando ] = useState(false);
+
+   //Extraer Datos
+   const { datos, cotizacion } = resumen;
+
+   return (
+      <Contenedor>
+         <Header
+            titulo= 'Cotizador de Seguros'
+         />
+         <ContenedorFormulario>
+            <Formulario 
+               guardarResumen={guardarResumen}
+               guardarCargando={guardarCargando}
+            />
+            
+            { cargando ? <Spinner/> : null }
+            
+            { !cargando 
+               ?
+                  <Fragment>
+                     <Resumen
+                        datos={datos}
+                     />
+                     <Resultado
+                        cotizacion={cotizacion}
+                     />
+                  </Fragment>
+               :
+                  null
+            }
+         </ContenedorFormulario>
+      </Contenedor>
+   );
 }
 
 export default App;
